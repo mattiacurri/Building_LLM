88884e9180431f1f17524630b1d7223f9d28980c
mattiacurri <mattia2002@gmail.com>
2024-09-09 23:57:43 +0200

    Initialized SmolGPT as GPT-2 small model

--- smol_gpt.py
+++ smol_gpt.py
@@ -21,6 +21,7 @@ class SmolGPTModel(nn.Module):
         self.dropout = nn.Dropout(cfg["drop_rate"])
         self.transformers_layers = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
         self.ln = LayerNorm(cfg["embed_dim"])
+        # Final linear layer to come back to vocabulary size in order to produce logits
         self.out_head = nn.Linear(cfg["embed_dim"], cfg["vocab_size"], bias=False)
 
     def forward(self, x):
@@ -127,3 +128,42 @@ block = TransformerBlock(SMOLGPT_CONFIG_124M)
 out_block = block(x)
 print(f'Input shape: {x.shape}')
 print(f'Output shape: {out_block.shape}')
+
+# Now let's initialize the 124 mln parameter GPT model
+torch.manual_seed(123)
+
+import tiktoken
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+
+SmolGPT = SmolGPTModel(SMOLGPT_CONFIG_124M)
+
+out = SmolGPT(batch)
+print(f'Input batch: {batch}')
+print(f'Output shape: {out.shape}')
+print(out)
+
+total_params = sum(p.numel() for p in SmolGPT.parameters())
+print(f'Total parameters in SmolGPT: {total_params:,}')
+
+# Weight tying: the embedding layer and the final linear layer share the same weights, that's the reason we have 163 mln parameters instead of 124 mln
+# Better training and model performance according to Raschka
+total_params_gpt2 =  total_params - sum(p.numel() for p in SmolGPT.out_head.parameters())
+print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
+
+
+mha_params = sum(p.numel() for x in SmolGPT.transformers_layers for p in x.attention.parameters()) / 12
+ff_params = sum(p.numel() for x in SmolGPT.transformers_layers for p in x.feed_forward.parameters()) / 12
+print(f'Parameters in a single MHA: {mha_params:,}')
+print(f'Parameters in a single FF: {ff_params:,}')
+
+# Memory requirements
+total_size_bytes = total_params * 4 # Assuming each parameter is a 32-bit float
+total_size_mb = total_size_bytes / (1024 * 1024)
+print(f'Total memory requirements: {total_size_mb:.2f} MB')
