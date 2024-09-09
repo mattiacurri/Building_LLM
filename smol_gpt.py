80ed402bc694bb6639c289e201fa4c0b7ef6c97f
mattiacurri <mattia2002@gmail.com>
2024-09-09 23:24:49 +0200

    Implemented GELU activation function and FeedForward classes

--- smol_gpt.py
+++ smol_gpt.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 SMOLGPT_CONFIG_124M = {
     "vocab_size": 50257, # Vocabulary size
     "context_length": 1024, # Context size
-    "embedding_size": 768, # Embedding size
+    "embed_dim": 768, # Embedding size
     "n_layers": 12, # Number of layers
     "n_heads": 12, # Number of attention heads
     "drop_rate": 0.1, # Dropout rate
@@ -15,12 +15,12 @@ SMOLGPT_CONFIG_124M = {
 class SmolGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.token_embedding = nn.Embedding(cfg["vocab_size"], cfg["embedding_size"])
-        self.position_embedding = nn.Embedding(cfg["context_length"], cfg["embedding_size"])
+        self.token_embedding = nn.Embedding(cfg["vocab_size"], cfg["embed_dim"])
+        self.position_embedding = nn.Embedding(cfg["context_length"], cfg["embed_dim"])
         self.dropout = nn.Dropout(cfg["drop_rate"])
         self.transformers_layers = nn.Sequential(*[DummyTransformerLayer(cfg) for _ in range(cfg["n_layers"])])
-        self.ln = LayerNorm(cfg["embedding_size"])
-        self.out_head = nn.Linear(cfg["embedding_size"], cfg["vocab_size"], bias=False)
+        self.ln = LayerNorm(cfg["embed_dim"])
+        self.out_head = nn.Linear(cfg["embed_dim"], cfg["vocab_size"], bias=False)
 
     def forward(self, x):
         batch_size, sequence_length = x.size()
@@ -39,7 +39,7 @@ class DummyTransformerLayer(nn.Module):
         return x
 
 class LayerNorm(nn.Module):
-    def __init__(self, embedding_size):
+    def __init__(self, embed_dim):
         super().__init__()
         self.eps = 1e-5
         '''
@@ -50,8 +50,8 @@ class LayerNorm(nn.Module):
         
         By learning these parameters, the model can retain the flexibility to represent the original input distribution, while still benefiting from the normalization process.
         '''
-        self.scale = nn.Parameter(torch.ones(embedding_size))
-        self.shift = nn.Parameter(torch.zeros(embedding_size))
+        self.scale = nn.Parameter(torch.ones(embed_dim))
+        self.shift = nn.Parameter(torch.zeros(embed_dim))
     
     def forward(self, x):
         mean = x.mean(dim=-1, keepdim=True)
@@ -61,10 +61,32 @@ class LayerNorm(nn.Module):
         norm_x = (x - mean) / torch.sqrt(var + self.eps)
         return self.scale * norm_x + self.shift
 
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        # GELU(x) = x * phi(x), where phi(x) is the standard Gaussian cumulative distribution function
+        # For a faster approximation, we can use the following formula
+        return 0.5 * x * (1 + torch.tanh((torch.sqrt(2 / torch.pi) * (x + 0.044715 * torch.pow(x, 3)))))
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            # (batch_size, num_tokens, embed_dim) -> (batch_size, num_tokens, 4 * embed_dim) -> (batch_size, num_tokens, embed_dim)
+            nn.Linear(cfg["embed_dim"], 4 * cfg["embed_dim"]), # input: (2, 3, 768) -> output: (2, 3, 3072) 
+            GELU(), # input: (2, 3, 3072) -> output: (2, 3, 3072)
+            nn.Linear(cfg["embed_dim"] * 4, cfg["embed_dim"]) # input: (2, 3, 3072) -> output: (2, 3, 768)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
 # Test of LayerNorm
 torch.manual_seed(123)
 batch_example = torch.randn(2, 5)
-ln = LayerNorm(embedding_size=5)
+ln = LayerNorm(embed_dim=5)
 out_ln = ln(batch_example)
 mean = out_ln.mean(-1, keepdim=True)
 var = out_ln.var(-1, keepdim=True, unbiased=False)
