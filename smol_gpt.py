e209068cc3355394b0faf797ea244e039f96ff44
mattiacurri <mattia2002@gmail.com>
2024-09-10 18:18:21 +0200

    Fixed simple text generation

--- smol_gpt.py
+++ smol_gpt.py
@@ -171,18 +171,50 @@ print(f'Total memory requirements: {total_size_mb:.2f} MB')
 # Let's move on to how to generate text with SmolGPT
 # For now we will generate text based on the most probable token at each step
 # Further improvements can be made by sampling from the distribution of the logits (temperature, top-k, top-p sampling, ...)
-def generate_text(model, tokenizer, prompt, max_len=100):
-    model.eval()
-    prompt = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
-    with torch.no_grad():
-        for _ in range(max_len):
-            logits = model(prompt)
-            logits = logits[:, -1, :] 
-            probs = torch.softmax(logits, dim=-1)
-            next_token = torch.argmax(probs, dim=-1)
-            prompt = torch.cat([prompt, next_token.unsqueeze(0)], dim=-1)
-    return prompt
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (B, T) array of indices in the current context
+    for _ in range(max_new_tokens):
 
-prompt = "Hello, I am a model"
-generated = generate_text(SmolGPT, tokenizer, prompt, max_len=4)
-print(f'Generated text: {tokenizer.decode(generated[0].tolist())}')
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        # Focus only on the last time step
+        # (batch, n_token, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]
+
+        # Get the idx of the vocab entry with the highest logits value
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
+start_context = "Hello, I am"
+
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+SmolGPT.eval() # disable dropout
+
+out = generate_text_simple(
+    model=SmolGPT,
+    idx=encoded_tensor, 
+    max_new_tokens=6, 
+    context_size=SMOLGPT_CONFIG_124M["context_length"]
+)
+
+print("Output:", out)
+print("Output length:", len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
