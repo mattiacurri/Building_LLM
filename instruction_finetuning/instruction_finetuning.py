ad6f3ffd1c770ee42e0667e57eb4ba76103442cc
Mattia Curri <mattia2002@gmail.com>
2024-09-23 19:29:07 +0200

    Fix training loop import in instruction_finetuning

--- instruction_finetuning/instruction_finetuning.py
+++ instruction_finetuning/instruction_finetuning.py
@@ -14,6 +14,9 @@ from functools import partial
 from torch.utils.data import Dataset, DataLoader
 import torch
 
+if os.path.dirname(os.path.dirname(os.path.abspath(__file__))) not in sys.path:
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from training.training_loop import plot_losses
 
 import psutil
